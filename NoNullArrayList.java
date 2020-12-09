086045bbb422fc0bbd2801fcecacee347d8922ae
Aries <aho11@stuy.edu>
2020-12-09 14:18:50 -0500

    created the constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,7 @@
+import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
+  private ArrayList<T> data;
 
+  public NoNullArrayList(){  }
+  public NoNullArrayList(int initialCapacity){}
 }
