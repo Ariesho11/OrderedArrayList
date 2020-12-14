e6416ceab2c0679fa49fd5ed90adb262b5d10450
Aries <aho11@stuy.edu>
2020-12-13 22:40:44 -0500

    added the methods and implemented the constructors

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,17 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList(){}
+  public OrderedArrayList(int initialCapacity){
+    super(initialCapacity);
+  }
 
+  public T set(int index, T element){
+    return element;
+  }
+
+  public boolean add(T element){
+    return true;
+  }
+  public void add(int index, T element){
+    
+  }
 }
