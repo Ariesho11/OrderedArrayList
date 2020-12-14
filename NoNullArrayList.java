6c60873c6ed3ecca6c9c3e0131f5a5063baa6447
Aries <aho11@stuy.edu>
2020-12-13 22:16:17 -0500

    implemented the constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,8 +2,10 @@ import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
   private ArrayList<T> data;
 
-  public NoNullArrayList(){  }
-  public NoNullArrayList(int initialCapacity){}
+  public NoNullArrayList(){}
+  public NoNullArrayList(int initialCapacity){
+    super(initialCapacity);
+  }
 
   public String set(int index, String element){
     return"";
@@ -12,7 +14,7 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public boolean add(String element){
     return true;
   }
-  
+
   public void add(int index, String element){
   }
 }
